f90ce546b58255af8185ff887439b33547a050bc
thejoshbq <j.boquiren98@gmail.com>
2025-04-04 08:23:57 -0400

    Removed unecessary line calling for REACHER CLI

--- lib/src/reacher/interface/__init__.py
+++ lib/src/reacher/interface/__init__.py
@@ -1,5 +1,4 @@
 # src/reacher/interface/__init__.py
-from .cli import main as run_cli
 from .local_dashboard import Dashboard as LocalDashboard
 from .network_dashboard import Dashboard as NetworkDashboard
 
